55818e7b539fd9ccf8f3850bd5684e11124c102b
Temi Michael <olasehindemichael285@gmail.com>
2022-10-27 02:45:35 +0100

    first commit

---
+++ PORTFOLIO WEBSITE/js/contact.js
@@ -0,0 +1,5 @@
+let n = "Temi Michael";
+let a = 20;
+let s = "RECTEM";
+let c = "ND2";
+console.log(`My name is ${n} and i am ${a}years of age currently in ${s} and i am in ${c} final year`);
---
+++ PORTFOLIO WEBSITE/js/index.js
@@ -0,0 +1,5 @@
+// alert ("Do you really want to leave this page");
+name = 'Temi';
+age = 20;
+school = "RECTEM";
+console.log('My name is ' + name + " and i am " + age +'years of age and also attend ' + school);
