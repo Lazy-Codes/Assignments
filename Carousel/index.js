f0a8afddc104f40d6f56b5c06603e6cf9a488e3b
Temi Michael <olasehindemichael285@gmail.com>
2022-11-08 04:18:24 +0100

    Update

---
+++ Carousel/index.js
@@ -0,0 +1,7 @@
+const slides = document.getElementsByClassName("carousel-item");
+const nextButton = document.getElementById("carousel-button-next");
+const prevButton = document.getElementById("carousel-button-prev");
+const dots = document.getElementsByClassName("dot");
+let position = 0;
+const numberOfSlides = slides.length;
+
